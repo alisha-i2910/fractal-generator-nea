fb705b810c6b4982792d925f5e9679c0d0b18f57
alisha <lishirf@gmail.com>
2025-02-05 13:17:16 +0000

    prehorizontallying

--- fractalgenerator/src/App.js
+++ fractalgenerator/src/App.js
@@ -6,6 +6,18 @@ function App() {
   const [coordY, setY] = useState(0);
   const canvasReference = useRef(null);
 
+  //Since it's so slow, make a callback function with a delay
+  //The 'callback' will be the function we fetch, so hoverEvent
+
+  function delayCoords(cb, delay=800){
+    let timeout
+    return(...args) => {
+      clearTimeout(timeout)
+      timeout = setTimeout(() => {
+        cb(...args)
+      }, delay) 
+    }
+  }
   const hoverEvent = (event) => {
     if (!canvasReference.current) return;
     const rect = canvasReference.current.getBoundingClientRect();
@@ -18,10 +30,13 @@ function App() {
     console.log("hoverEvent triggered!"); 
   };
 
+  //debouncing this
+  const debouncehoverEvent = delayCoords(hoverEvent)
+
   return (
     <div>
       <h1>08001111</h1>
-      <Canvas ref={canvasReference} hoverEvent={hoverEvent} />
+      <Canvas ref={canvasReference} hoverEvent={debouncehoverEvent} />
       <h1>{coordX.toFixed(2)}, {coordY.toFixed(2)}</h1>
     </div>
   );
